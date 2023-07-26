e289faa00ed73b2f2ec38911c601256caf01e7fa
t1907 <73695370+t1907@users.noreply.github.com>
2023-07-26 16:34:46 +0200

    Update wolacz_dzwiek.js

--- wolacz_dzwiek.js
+++ wolacz_dzwiek.js
@@ -23,27 +23,27 @@ tSound.autoplay = false;
 function run(Engine) {
     if (Engine && Engine.npcs && Engine.npcs.check) window.API.addCallbackToEvent("newNpc", function(npc) {
         if (npc.d.wt > 19 && npc.d.wt <=29) {
-            eSound.volume = 0.20;
+            eSound.volume = 0.30;
             eSound.play();
             setTimeout(changeTitleToPrevious, 1000);
         }
         if (npc.d.wt > 79 && npc.d.wt <=99) {
             if(Engine.hero.d.clan){
-                _g('chat', false, {
-                    c: `/k Znaleziono herosa ${npc.d.nick} (${npc.d.lvl}lvl) na mapie ${Engine.map.d.name} (${npc.d.x},${npc.d.y})`
-                })
+                _g('chat&channel=clan', !1,{
+                c: `Znaleziono herosa ${npc.d.nick} (${npc.d.lvl}lvl) na mapie ${Engine.map.d.name} (${npc.d.x},${npc.d.y})`
+            });
             }
-            hSound.volume = 0.20;
+            hSound.volume = 0.30;
             hSound.play();
             setTimeout(changeTitleToPrevious, 1000);
         }
-        if (npc.d.wt > 99 && Engine.map.mode !==5) {
-            if(Engine.hero.d.clan){
-                _g('chat', false, {
-                    c: `/k Znaleziono tytana ${npc.d.nick} (${npc.d.lvl}lvl) na mapie ${Engine.map.d.name} (${npc.d.x},${npc.d.y})`
-                })
+        if (npc.d.wt > 99) {
+            if(Engine.hero.d.clan && Engine.map.mode !==5){
+                _g('chat&channel=clan', !1,{
+                c: `Znaleziono tytana ${npc.d.nick} (${npc.d.lvl}lvl) na mapie ${Engine.map.d.name} (${npc.d.x},${npc.d.y})`
+            });
             }
-            tSound.volume = 0.20;
+            tSound.volume = 0.30;
             tSound.play();
             setTimeout(changeTitleToPrevious, 1000);
         }
