3f8a43ea68a64b1c3afef582d46a0e1a4d25812d
t1907 <73695370+t1907@users.noreply.github.com>
2024-01-02 10:45:37 +0100

    Update wolacz_dzwiek.js

--- wolacz_dzwiek.js
+++ wolacz_dzwiek.js
@@ -11,30 +11,45 @@ function changeTitleToPrevious()
   document.title = "Margonem MMORPG";
 }
 
-var eSound = new Audio("https://margonemskie-kalkulatory.opx.pl/DSE.mp3");
-eSound.autoplay = false;
+var elite2Sound = new Audio("https://margonemskie-kalkulatory.opx.pl/DSE.mp3");
+elite2Sound.autoplay = false;
 
-var hSound = new Audio("https://raw.githubusercontent.com/clasicker/addons/master/WH/detect.mp3");
-hSound.autoplay = false;
+var heroSound = new Audio("https://raw.githubusercontent.com/clasicker/addons/master/WH/detect.mp3");
+heroSound.autoplay = false;
+
+var titanSound = new Audio("https://raw.githubusercontent.com/clasicker/addons/master/WH/detect.mp3");
+titanSound.autoplay = false;
 
 function run(Engine) {
-    var justCalled = [];
+    var autoNotify = true;
+    var alreadyNotify = [];
 
     if (Engine && Engine.npcs && Engine.npcs.check) window.API.addCallbackToEvent("newNpc", function(npc) {
-        if (npc.d.wt > 19 && npc.d.wt <=29 || npc.d.nick === "Erktos") {
-            eSound.volume = 0.30;
-            eSound.play();
+        if (npc.d.wt > 19 && npc.d.wt <=79) {
+            elite2Sound.volume = 0.30;
+            elite2Sound.play();
             setTimeout(changeTitleToPrevious, 1000);
         }
-        if (npc.d.wt > 79 && npc.d.wt <=99 || npc.d.wt > 99) {
-            if(Engine.hero.d.clan && !justCalled.includes(npc.d.id)){
+        if (npc.d.wt > 79 && npc.d.wt <=99) {
+            if(Engine.hero.d.clan && autoNotify && !alreadyNotify.includes(npc.d.id)){
                 _g('chat&channel=clan', !1,{
-                c: `${npc.d.nick} (${npc.d.lvl}lvl) na mapie ${Engine.map.d.name} (${npc.d.x},${npc.d.y})`
+                c: `Heros ${npc.d.nick} (${npc.d.lvl}lvl) na mapie ${Engine.map.d.name} (${npc.d.x},${npc.d.y})`
             });
             }
-            justCalled.push(npc.d.id);
-            hSound.volume = 0.30;
-            hSound.play();
+            alreadyNotify.push(npc.d.id);
+            heroSound.volume = 0.30;
+            heroSound.play();
+            setTimeout(changeTitleToPrevious, 1000);
+        }
+        if (npc.d.wt > 99 ) {
+            if(Engine.hero.d.clan && autoNotify && !alreadyNotify.includes(npc.d.id)){
+                _g('chat&channel=clan', !1,{
+                c: `Tytan ${npc.d.nick} (${npc.d.lvl}lvl) na mapie ${Engine.map.d.name} (${npc.d.x},${npc.d.y})`
+            });
+            }
+            alreadyNotify.push(npc.d.id);
+            titanSound.volume = 0.30;
+            titanSound.play();
             setTimeout(changeTitleToPrevious, 1000);
         }
     })
