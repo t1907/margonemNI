ea8f2067436a6aacda0a5b2de773aee6d8e8674e
t1907 <73695370+t1907@users.noreply.github.com>
2024-02-23 08:55:18 +0100

    Update groove.js

--- groove.js
+++ groove.js
@@ -11,7 +11,7 @@ GM_addStyle ( `
 
 .cll-timer{font-family: Arial, Helvetica, sans-serif !important;}
 .cll-timer-monster {color: #57e106 !important;}
-//.cll-timer-highlighted {background: #93828242 !important}
+//.cll-timer-highlighted {background: #0a1f42 !important}
 .cll-timer-monster[style]{color: red !important;}
 
 ` );
