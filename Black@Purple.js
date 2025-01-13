98fef178399a58faf95028f410a925a0b0fa9dc4
t1907 <73695370+t1907@users.noreply.github.com>
2025-01-13 13:46:36 +0100

    upp

--- Black@Purple.js
+++ Black@Purple.js
@@ -80,7 +80,7 @@
         .loot-window .button-holder .button.not{background:linear-gradient(to top, #101010, #101010)}
         .loot-window .button-holder .button.not:before{box-shadow:inset 0 0 1px 1px #101010}
         .loot-window .button-holder .button.must{background:linear-gradient(to top, #101010, #101010)}
-        .loot-window .items-wrapper .loot-item-wrapper .button-holder .button.green{border-color: #40ff40;}
+        .loot-window .items-wrapper .loot-item-wrapper .button-holder .button.green{border-color: #101010;}
 
         .popup-menu{box-shadow:none; background-color: #00000000; border-radius: 1px;}
         .popup-menu .menu-item {background-color: #101010; border: 1px solid #808080; border-radius: 1px;}
@@ -107,7 +107,7 @@
         .button, .widget-button {border-radius: 0px !important;}
         .button, .widget-button::after {border-radius: 0px !important;}
         
-        .content .loot-window .items-wrapper .loot-item-wrapper .button-holder .button.green:not(.pressed){border-color: #40ff40; box-shadow:inset 0 0 1px 1px #40ff40 !important;}
+        .content .loot-window .items-wrapper .loot-item-wrapper .button-holder .button.green:not(.pressed){border-color: #101010; box-shadow:inset 0 0 1px 1px #40ff40 !important;}
 
         .addons-panel .main-header, .left-grouped-list-and-right-description-window .main-header {background: url(https://raw.githubusercontent.com/t1907/margonemNI/refs/heads/main/images/1.png)}
 
