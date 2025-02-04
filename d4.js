7bf3b33db7189683b9893b38750e99414b75c2e8
t1907 <73695370+t1907@users.noreply.github.com>
2025-02-04 13:59:24 +0100

    ok

--- d4.js
+++ d4.js
@@ -87,7 +87,9 @@
        .ni-input input.default {border: 1px solid #640064; background: #101010; color: #f5f5f5; box-shadow: none; border-radius: 0px;}
        input.default {border: 1px solid #640064; background: #101010; color: #f5f5f5; box-shadow: none; border-radius: 0px;}
 
-       .loot-window .items-wrapper .loot-item-wrapper{box-shadow: 0px 0px 0px 1px #640064;}
+       .loot-window .items-wrapper .loot-item-wrapper{box-shadow: 0px 0px 0px 1px #640064; border: 1px solid #64006400;}
+       .loot-window .items-wrapper .loot-item-wrapper .slot {border: 1px solid #87573c00}
+       .loot-window .items-wrapper .loot-item-wrapper .text-info {border-top: 1px solid #101010;border-bottom: 1px solid #101010;}
 
        .tw-list-item{background: #101010 !important;}
        .border-window.transparent .border-image{box-shadow: 0 0 0 1px #000, 0 0 0 2px #640064, 0 0 0 3px #000000, 2px 2px 3px 3px #000; border-radius: 0px;}
