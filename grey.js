d63533441616fe3c10e606782dd5a106a20aa1ce
t1907 <73695370+t1907@users.noreply.github.com>
2025-02-11 20:16:05 +0100

    ok

--- grey.js
+++ grey.js
@@ -65,12 +65,12 @@
   
         .depo .bottom-section .cards-menu .card::before {border-image: url(https://raw.githubusercontent.com/t1907/margonemNI/refs/heads/main/grey/width-card-button-active.png) 17 20 fill repeat}
   
-        .interface-element-active-card-background, .tabs-nav .card.active, .matchmaking-ranking .cards-header-wrapper .cards-header .active, 
+        .tabs-nav .card.active, .matchmaking-ranking .cards-header-wrapper .cards-header .active, 
         .cards-header-wrapper .cards-header .card.active {background: url(https://raw.githubusercontent.com/t1907/margonemNI/refs/heads/main/grey/width-card-button.png) no-repeat; background-size: 100% 100%;}
        
         .depo .bottom-section .cards-menu .card:not(.disabled).active::before {border-image: url(https://raw.githubusercontent.com/t1907/margonemNI/refs/heads/main/grey/width-card-button.png) 17 20 fill repeat}
   
-        .interface-element-active-card-background-stretch {border-image: url(https://raw.githubusercontent.com/t1907/margonemNI/refs/heads/main/grey/width-card-button.png) 5 fill repeat stretch;}
+        .interface-element-active-card-background, .interface-element-active-card-background-stretch {border-image: url(https://raw.githubusercontent.com/t1907/margonemNI/refs/heads/main/grey/width-card-button.png) 5 fill repeat stretch;}
   
        .drop-down-menu-section .type-header {background: url(https://raw.githubusercontent.com/t1907/margonemNI/refs/heads/main/grey/auctions/header_left.png);}
   
