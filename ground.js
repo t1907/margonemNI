9bcb01ad89f3f9148d85031d9d126ea56edde74d
t1907 <73695370+t1907@users.noreply.github.com>
2025-02-23 20:09:16 +0100

    up

--- ground.js
+++ ground.js
@@ -21,7 +21,7 @@
     .layer.interface-layer .main-column.right-column {background: url('https://raw.githubusercontent.com/t1907/margonemNI/refs/heads/main/ground/oTLO9A2.png') -251px 0px !important;}
     .layer.interface-layer .positioner.top .bg{background: url('https://raw.githubusercontent.com/t1907/margonemNI/refs/heads/main/ground/gora.png') 0 -61px repeat !important;}
     .layer.interface-layer .positioner.top .hud-container .map_ball {background: url(https://i.imgur.com/y8yj4E7.png)}
-    .layer.interface-layer .positioner.bottom .bg{background: url('https://raw.githubusercontent.com/t1907/margonemNI/refs/heads/main/ground/VW7joVD.png') 0 0 repeat !important;}
+    .layer.interface-layer .positioner.bottom .bg{background: url('https://raw.githubusercontent.com/t1907/margonemNI/refs/heads/main/ground/dol.png') 0 0 repeat !important;}
     .layer.interface-layer .positioner.bottom .bg-additional-widget-left {background: #101010;}
     .layer.interface-layer .positioner.bottom .bg-additional-widget-right {background: #101010;}
     .layer.interface-layer .main-column.right-column .border {background: transparent !important;}
