c0799ea2680abb0396b6332c4339fbc4dd645741
t1907 <73695370+t1907@users.noreply.github.com>
2025-03-28 21:42:33 +0100

    ok

--- purple.js
+++ purple.js
@@ -141,7 +141,7 @@
   
   .battle-pass-window .section .scroll-wrapper .battle-pass-bg, .bonus-selector-wnd__bg, .bonus-reselect-wnd__bg{background-size: contain;}
   
-  .tw-list-item{background: #101010 !important;}
+  .tw-list-item{background: #101010 !important; border-radius: 0; box-shadow: 0 0 0 1px rgb(100 0 100 / 23%) inset, 0 0 0 1px rgba(0, 0, 0, 0.55)}
   .border-window.transparent .border-image, .dialogue-window .border-image{box-shadow: 0 0 0 1px #640064; border-radius: 0;}
   .border-window.transparent .search-wrapper{border-top:1px solid rgba(100,0,100,.5)}
   
